ab935dd24c932f3faa602d97c53103939b289719
KillingForm <asdf>
2021-07-17 18:11:16 -0400

    finished load animation and background

--- app/components/load-animation.js
+++
@@ -1,6 +0,0 @@
-import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-export default class LoadAnimationComponent extends Component {
-
-  @tracked animationRunning = true;
-}
