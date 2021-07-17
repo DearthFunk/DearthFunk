463943cfa93ef0a5a800e115e595c7789034131f
KillingForm <asdf>
2021-07-17 16:36:46 -0400

    add background animation and stopped text animation on click

--- app/components/load-animation.js
+++ app/components/load-animation.js
@@ -1,4 +1,6 @@
 import Component from '@glimmer/component';
-
+import { tracked } from '@glimmer/tracking';
 export default class LoadAnimationComponent extends Component {
+
+  @tracked animationRunning = true;
 }
