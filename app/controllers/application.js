8142dd08af53069e88f39bc6245703a4b61b799c
KillingForm <asdf>
2021-07-25 19:35:33 -0400

    chnaged up the loading animation

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,5 +2,5 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 
 export default class ApplicationController extends Controller {
-  @tracked running = true;
+  @tracked loading = true;
 }
