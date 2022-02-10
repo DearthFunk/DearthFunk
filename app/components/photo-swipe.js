77f45d64b059e092c9699b0aabf6db78c13b6ec1
KillingForm <asdf>
2022-02-10 01:33:06 -0500

    fix: styling for photo-swipe ? hopefully

--- app/components/photo-swipe.js
+++
@@ -1,7 +0,0 @@
-import PhotoSwipeComponent from 'ember-photoswipe/components/photo-swipe';
-
-export default class extends PhotoSwipeComponent {
-  history = false;
-  bgOpacity = 0.7;
-  shareButtons = [];
-}
